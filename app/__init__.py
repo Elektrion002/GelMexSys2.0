2a5e79f5585854cd5e19afabdf39575939afcf9b
edwared <elektrion@gmail.com>
2025-12-27 15:31:02 -0600

    Feat: Creacion de Endpoints API (v1) para validacion de lectura de datos desde PostgreSQL.

--- app/__init__.py
+++ app/__init__.py
@@ -4,31 +4,21 @@ from config import config
 from app.extensions import db
 
 def create_app(config_name='default'):
-    # 1. Instanciar Flask
     app = Flask(__name__)
-
-    # 2. Cargar Configuración (Dev, Prod, etc.)
     app.config.from_object(config[config_name])
 
-    # 3. Conectar Base de Datos
     db.init_app(app)
 
-    # 4. Cargar Modelos (Contexto de Aplicación)
-    # Esto es CRÍTICO: Si no importamos los modelos aquí, SQLAlchemy no sabrá que existen
-    # y no creará las tablas.
     with app.app_context():
-        from app.models import catalogs
-        from app.models import users
-        from app.models import infrastructure
-        from app.models import products
-        from app.models import clients
+        # Importamos modelos para que SQLAlchemy sepa de ellos
+        from app.models import catalogs, users, infrastructure, products, clients
         
-        # Opcional: Crear tablas automáticamente si no existen (Solo desarrollo)
-        # En producción usaremos Migraciones (Alembic)
+        # OJO: Ya no necesitamos db.create_all() siempre, porque ya existen.
+        # Pero no estorba dejarlo por seguridad en desarrollo.
         db.create_all()
 
-    # 5. Registrar Blueprints (Rutas) - Lo haremos más adelante
-    # from app.blueprints.home import home_bp
-    # app.register_blueprint(home_bp)
-
+    # --- AQUÍ REGISTRAMOS EL NUEVO BLUEPRINT ---
+    from app.blueprints.api_test import api_bp
+    app.register_blueprint(api_bp)
+    
     return app
